4d155aaf6d7cf4006d6b296aa591f229283bca19
Colin330 <colin56rosmord@gmail.com>
2026-03-23 15:20:28 +0100

    creation page web affichage artiste BDD

--- app/app.py
+++ app/app.py
@@ -1,8 +1,23 @@
 from flask import Flask
- 
+import pymysql
+
 app = Flask(__name__)
  
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    conn = pymysql.connect(
+        user = "root",
+        password = "uimm",
+        host = "localhost",
+        port =3306,
+        database= "spectacle")
+    cursor= conn.cursor()
+    cursor.execute ('SELECT * from Artistes')
+    result = cursor.fetchone()
+    
+    return f"<p>Hello, World! {result}</p>"
 
+
+@app.route("/voir_artiste")
+def voir_artiste():
+    
